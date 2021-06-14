b47e297a0109eadd82770401cd63fbfb825e2846
younghoonkwon <younghoon.kwon91@gmail.com>
2021-06-14 15:45:40 +0900

    initialize

---
+++ src/main/java/com/gumlayudong/gumlayudongbackend/GumlayudongBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gumlayudong.gumlayudongbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GumlayudongBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GumlayudongBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gumlayudong/gumlayudongbackend/GumlayudongBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gumlayudong.gumlayudongbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GumlayudongBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
