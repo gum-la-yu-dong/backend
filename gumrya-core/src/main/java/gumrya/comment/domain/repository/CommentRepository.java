e2849e69c097c881ce2cf3e416315db4dc6e9be7
TaeJeong Kim <xowjd41@naver.com>
2021-06-27 15:11:39 +0900

    reafctor: yml 멀티 모듈 적용

---
+++ gumrya-core/src/main/java/gumrya/comment/domain/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package gumrya.comment.domain.repository;
+
+import gumrya.comment.domain.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
